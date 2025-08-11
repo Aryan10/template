e47b4f670e661da97d4b940ccdb3aa5ccb49d95d
Aryan10 <aryan.shivam.10@gmail.com>
2025-08-11 14:20:11 +0530

    Use C++20 concept for container I/O and default Codeforces template

--- include/utility/IOStream.cpp
+++ include/utility/IOStream.cpp
@@ -1,16 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename K, typename V> istream& operator >> (istream& is, pair<K, V>& p) {
+template <typename K, typename V>
+istream& operator>>(istream& is, pair<K, V>& p) {
     return is >> p.first >> p.second;
 }
 
-template <typename T> istream& operator >> (istream& is, vector<T>& v) {
-    for (T& i : v) is >> i;
+template <typename C>
+concept Container = requires(C c) {
+    typename C::value_type;
+    begin(c);
+    end(c);
+} and not same_as<remove_cvref_t<C>, string>;
+
+template <Container C>
+istream& operator>>(istream& is, C& c) {
+    for (auto& x : c) 
+        is >> x;
     return is;
 }
 
-template <typename T> ostream& operator << (ostream& os, vector<T> const& v) {
-    for (T const& i : v) os << i << " ";
+template <Container C>
+ostream& operator<<(ostream& os, const C& c) {
+    for (auto const& x : c) 
+        os << x << ' ';
     return os;
 }
--- src/codeforces.cpp
+++ src/codeforces.cpp
@@ -1,23 +1,39 @@
-#pragma GCC optimize("O3,unroll-loops")
 #pragma region ultraaryan10
+
+#pragma GCC optimize("O3,unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 #define int long long
 // #define SINGLE_TEST_CASE
 // #define FLOATING_POINT_IO
 // #define INTERACTIVE_IO
 
-
+/* MACRO */
 #define all(v) std::begin(v), std::end(v)
 #define len(v) (static_cast<int>((v).size()))
 
+/* CONTAINERS & I/O */
+template <typename T, typename CMP = less<void>>
+using ordered_set = tree<T, null_type, CMP, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename K, typename V> istream& operator >> (istream& is, pair<K, V>& p) { return is >> p.first >> p.second; }
+template <typename C> concept Container = requires(C c) { typename C::value_type; begin(c); end(c); } and not same_as<remove_cvref_t<C>, string>;
+template <Container C> istream& operator>>(istream& is, C& c) { for (auto& x : c) is >> x; return is; }
+template <Container C> ostream& operator<<(std::ostream& os, const C& c) { for (auto const& x : c) os << x << ' '; return os; }
+
+/* PRINT & DEBUG */
 template<typename... Args> void print(Args&&... args) { ((cout << args << ' '), ...); }
 template<typename... Args> void println(Args&&... args) { print(args..., '\n'); }
 #ifndef ONLINE_JUDGE
 template<typename... Args> void debug(Args&&... args) { ((cerr << args << ' '), ...); cerr << '\n'; }
 #endif
-void solve();
 
+/* MAIN */
+void solve();
 signed main() {
     #ifndef INTERACTIVE_IO
         #define endl '\n'
@@ -33,6 +49,8 @@ signed main() {
         solve();
 }
 #pragma endregion
+
+
 
 void solve() {
     
