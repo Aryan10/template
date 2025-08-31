9e89f800977fbb9b0126b474b957fa91f9df4b7c
Aryan10 <aryan.shivam.10@gmail.com>
2025-08-31 14:04:26 +0530

    New way to handle single test cases and DEBUG macro for printing variable names

--- src/codeforces.cpp
+++ src/codeforces.cpp
@@ -1,56 +1,47 @@
-#pragma region ultraaryan10
-
 #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma region ultraaryan10
 #include <bits/stdc++.h>
 using namespace std;
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
-#define int long long
-// #define SINGLE_TEST_CASE
-// #define FLOATING_POINT_IO
-// #define INTERACTIVE_IO
-
-/* MACRO */
-#define all(v) std::begin(v), std::end(v)
-#define len(v) (static_cast<int>((v).size()))
-
-/* CONTAINERS & I/O */
 template <typename T, typename CMP = less<void>>
 using ordered_set = tree<T, null_type, CMP, rb_tree_tag, tree_order_statistics_node_update>;
 
-template <typename K, typename V> istream& operator >> (istream& is, pair<K, V>& p) { return is >> p.first >> p.second; }
-template <typename C> concept Container = requires(C c) { typename C::value_type; begin(c); end(c); } and not same_as<remove_cvref_t<C>, string>;
-template <Container C> istream& operator>>(istream& is, C& c) { for (auto& x : c) is >> x; return is; }
-template <Container C> ostream& operator<<(std::ostream& os, const C& c) { for (auto const& x : c) os << x << ' '; return os; }
+#define int long long
 
-/* PRINT & DEBUG */
-template<typename... Args> void print(Args&&... args) { ((cout << args << ' '), ...); }
-template<typename... Args> void println(Args&&... args) { ((cout << args << ' '), ...); cout << '\n'; }
+/* MACRO */
+#define endl '\n'
+#define all(v) std::begin(v), std::end(v)
+#define len(v) (static_cast<int>((v).size()))
+
+/* I/O */
+template <class C> concept Container = requires(C c) { typename C::value_type; begin(c); end(c); } and not same_as<remove_cvref_t<C>, string>;
+template <Container C> istream& operator >> (istream& is, C& c) { for (auto& x : c) is >> x; return is; }
+template <Container C> ostream& operator << (ostream& os, C const& c) { for (auto const& x : c) os << x << ' '; return os; }
+template <class K, class V> istream& operator >> (istream& is, pair<K, V>& p) { return is >> p.first >> p.second; }
+template <class K, class V> ostream& operator << (ostream& os, pair<K, V> const& p) { return os << p.first << ' ' << p.second << '\n'; }
+template <class... Args> void print(Args&&... args) { ((cout << args << ' '), ...); }
+template <class... Args> void println(Args&&... args) { ((cout << args << ' '), ...); cout << '\n'; }
+
 #ifndef ONLINE_JUDGE
-template<typename... Args> void debug(Args&&... args) { ((cerr << args << ' '), ...); cerr << '\n'; }
+    #define DEBUG(x) cerr << #x << " = " << (x) << endl;
+    template <class... Args> void debug(Args&&... args) { ((cerr << args << ' '), ...); cerr << '\n'; }
 #endif
 
 /* MAIN */
 void solve();
+bool __single_tc;
+#define final auto __do_single = [] () { return __single_tc = true; } ();
 signed main() {
-    #ifndef INTERACTIVE_IO
-        #define endl '\n'
-        cin.tie(0)->sync_with_stdio(0);
-    #endif
-    #ifdef FLOATING_POINT_IO
-        #define double long double
-        cout << fixed << setprecision(20);
-    #endif
-    #ifndef SINGLE_TEST_CASE
-        signed TC{1}; cin >> TC; while (TC--)
-    #endif
-        solve();
+    cin.tie(0)->sync_with_stdio(0);
+    signed TC{1}; 
+    if (!__single_tc) cin >> TC; 
+    while (TC--) solve();
 }
 #pragma endregion
-
-
 
 void solve() {
     
